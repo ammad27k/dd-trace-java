c89f69e7dab48d7c26dab5d650e2a7efcc0d476d
ammad27k <ammad27k@gmail.com>
2024-08-20 04:06:50 +0200

    Update FieldBackedContextInjector.java

--- dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/context/FieldBackedContextInjector.java
+++ dd-java-agent/agent-tooling/src/main/java/datadog/trace/agent/tooling/context/FieldBackedContextInjector.java
@@ -30,7 +30,7 @@ import net.bytebuddy.pool.TypePool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** Injects fields and accessors so the class can act as a surrogate {@link ContextStore}. */
+/** Injects fields and accessors so the class can act as a surrogate {@link ContextStore}. Test */
 public final class FieldBackedContextInjector implements AsmVisitorWrapper {
 
   private static final Logger log = LoggerFactory.getLogger(FieldBackedContextInjector.class);
